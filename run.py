99c3b76503d40be76560f643471b151c0aaded62
Gguidini <giovanni.guidini@gmail.com>
2020-10-05 09:32:26 -0300

    Refactor simulation start, wrapping in Simulator class

--- run.py
+++ run.py
@@ -1,17 +1,5 @@
 import pyglet
 import sys
-import json
-import simpy
-import pathlib
-import esper
-
-from typing import NamedTuple
-
-import map_parser
-
-from components.Map import Map
-from components.Renderable import Renderable
-from components.Inventory import Inventory
 
 from systems.MovementProcessor import MovementProcessor
 from systems.CollisionProcessor import CollisionProcessor
@@ -25,70 +13,48 @@ import systems.EnergyConsumptionDESProcessor as energySystem
 import systems.ManageObjects as ObjectManager
 import systems.ClawDESProcessor as ClawProcessor
 
-EVENT = NamedTuple('Event', [('type', str), ('payload', object)])
+from main import Simulator, EVENT
 
-FPS = 60
-DEFAULT_LINE_WIDTH = 10
-CONFIG = 'simulation.json' if len(sys.argv) == 1 else sys.argv[1]
+# Create a simulation with config
+simulator = Simulator(sys.argv[1])
+# Some simulator objects
+width, height = simulator.window_dimensions
+window = simulator.window
+eventStore = simulator.KWARGS['EVENT_STORE']
 
-with open(CONFIG) as fd:
-    config = json.load(fd)
-FILE = pathlib.Path(config.get('context', '.')) / config.get('map', 'map.drawio')
-SIMULATION_DURATION = config.get('duration', -1)
+# Defines and initializes esper.Processor for the simulation
+normal_processors = [
+    MovementProcessor(minx=0, miny=0, maxx=width, maxy=height),
+    CollisionProcessor(),
+    RenderProcessor(),
+    PathProcessor()
+]
+# Defines DES processors
+des_processors = [
+    ClawProcessor,
+    ObjectManager,
+    StopCollision,
+    gotoProcessor,
+    mapProcessor,
+    energySystem
+]
+# Add processors to the simulation, according to processor type
+for p in normal_processors:
+    simulator.add_system(p)
+for p in des_processors:
+    simulator.add_DES_system(p)
 
-simulation = map_parser.build_simulation_from_map(FILE)
-world: esper.World = simulation['world']
-window = simulation['window']
-batch = simulation['batch']
-(window_name, (WIDTH, HEIGHT), BKGD) = simulation['window_props']
-draw2ent = simulation['draw_map']
-objects = simulation['objects']
-# Global inventory
-interactive = world.component_for_entity(1, Inventory).objects
-
-print(f"==> Simulation objects")
-for id, objId in objects:
-    entity = draw2ent.get(objId)
-    print(f"OBJ #{id} (draw {objId}). Type {entity[1]['type']}")
-    # print(f"Object has components {world.components_for_entity(id)}")
-    if world.has_component(id, Map):
-        ent_map = world.component_for_entity(id, Map)
-        print("\tAvailable paths:")
-        for idx, key in enumerate(ent_map.paths.keys()):
-            print(f"\t{idx}. {key}")
-robot1 = objects[0] if len(objects) > 0 else None
-
-# Create some Processor instances, and assign them to the World to be processed:
-movement_processor = MovementProcessor(
-    minx=0, miny=0, maxx=WIDTH, maxy=HEIGHT)
-world.add_processor(movement_processor)
-
-collision_processor = CollisionProcessor()
-world.add_processor(collision_processor)
-
-render_processor = RenderProcessor()
-world.add_processor(render_processor)
-
-path_processor = PathProcessor()
-world.add_processor(path_processor)
-
-################################################
-#  Set up pyglet events for input and rendering:
-################################################
-
+# pyglet related stuff
 KEYS = pyglet.window.key
 GOTO = False
 MAP = False
 buff = []
-
-
 @window.event
 def on_text(text):
     if GOTO and text != 'G':
         buff.append(text)
     elif MAP:
         buff.append(text)
-
 
 @window.event
 def on_key_press(key, mod):
@@ -134,7 +100,7 @@ def on_draw():
     # Clear the window to background color
     window.clear()
     # Draw the batch of Renderables:
-    batch.draw()
+    simulator.batch.draw()
 
 
 @window.event
@@ -144,55 +110,8 @@ def on_close():
     EXIT = False
 
 
-####################################################
-#  Schedule a World update and start the pyglet app:
-####################################################
-
-def simulation_loop(pass_switch_ref):
-    kwargs = {
-        "ENV": env,
-        "WORLD": world,
-        "_KILLSWITCH": (killswitch if pass_switch_ref else None),
-        "EVENT_STORE": eventStore,
-        # Pyglet specific things (for the re-create entity)
-        "BATCH": batch,
-        "WINDOW_OPTIONS": ((WIDTH, HEIGHT), DEFAULT_LINE_WIDTH)
-    }
-    # Discrete processors
-    env.process(gotoProcessor.process(kwargs))
-    env.process(mapProcessor.process(kwargs))
-    env.process(StopCollision.process(kwargs))
-    # env.process(energySystem.process(kwargs))
-    env.process(ObjectManager.process(kwargs))
-    env.process(ClawProcessor.process(kwargs))
-    # Other processors
-    while not EXIT:
-        pyglet.clock.tick()
-        world.process(kwargs)
-        # For many windows
-        for w in pyglet.app.windows:
-            w.switch_to()
-            w.dispatch_events()
-            w.dispatch_event('on_draw')
-            w.flip()
-        # ticks on the clock
-        switch = yield killswitch | env.timeout(1.0 / FPS, False)
-        if killswitch in switch:
-            break
-    return 0
-
-
-EXIT = False
 if __name__ == "__main__":
     # NOTE!  schedule_interval will automatically pass a "delta time" argument
     #        to world.process, so you must make sure that your Processor classes
     #        account for this. See the example Processors above.
-    env = simpy.Environment()
-    eventStore = simpy.FilterStore(env, capacity=10)
-    killswitch = env.event()
-    if SIMULATION_DURATION > 0:
-        env.process(simulation_loop(False))
-        env.run(until=SIMULATION_DURATION)
-    else:
-        env.process(simulation_loop(True))
-        env.run()
+    simulator.run()
