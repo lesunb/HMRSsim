ecc84f74d8cb5220f10fff987f996c04784f4026
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-02-24 11:30:30 -0300

    refactoring test robot displacement

--- tests/features/robot_displacement.feature
+++ tests/features/robot_displacement.feature
@@ -1,20 +1,26 @@
 Feature: Robot Displacement
 
+# Tested Features: movement using Path, movement using GotoPos
+
+# Acceptance Criteria - First Scenario: the robot is capable of following 
+# the path and entering the room.
+# Acceptance Criteria - Second Scenario: the robot is capable of go to a
+# specific position given a GotoPos Event.
+
+# Map: tests/data/three_room_map.drawio
+# Map description: consists of three rooms, a robot and a path that leads
+# the robot to the third room
+
 Scenario: The robot moves to a specific Position
-Given a map with three rooms
-"""
-Map consisting of three rooms, a robot and a path that leads the robot to
-the third room.
-tests/data/room3.drawio
-"""
-And a Position 2,2 in 'room3'
-When the robot receives a move-to Position 2,2 in 'room3'
-And after run simulation
-Then the robot is in Position 2,2 of 'room3'
+    Given a map with three rooms
+    And a robot with the ability to move to a specific position
+    And a goto event to the center of 'room three'
+    When after run simulation
+    Then the robot is in the center of 'room three'
 
 Scenario: The robot moves along a path until it reaches its destination
-Given a map with three rooms
-And a path to 'room3'
-When the robot receives a move-to end of path in 'room3'
-And after run simulation
-Then the robot is in the end of path in 'room3'
+    Given a map with three rooms
+    And a robot with the ability to follow a path
+    And a path from the robot to the center of 'room three'
+    When after run simulation
+    Then the robot is in the center of 'room three'
--- tests/step_defs/test_robot_displacement.py
+++ tests/step_defs/test_robot_displacement.py
@@ -1,4 +1,4 @@
-from tests.aux.testaux import get_component, store_goto_position_event, get_path_last_point
+from tests.aux.testaux import create_path, get_component, store_goto_position_event, get_center
 import pytest
 from pytest_bdd import scenarios, given, when, then
 from main import Simulator
@@ -21,48 +21,50 @@ def config():
     }
     return config
 
+@pytest.fixture
+def robot():
+    return "robot"
+
+
 @given("a map with three rooms", target_fixture="simulation") 
 def map(config):
-    config["map"] = "room3.drawio"
+    config["map"] = "three_room_map.drawio"
     simulation = Simulator(config)
     return simulation
 
-@given("a Position 2,2 in 'room3'")
-def position_in_2_2(simulation):
-    store_goto_position_event(simulation, 'robot', [2,2])
+@given("a robot with the ability to follow a path")
+def ability_to_follow_path(simulation):
+    width, height = simulation.window_dimensions
+    simulation.add_system(PathProcessor())
+    simulation.add_system(MovementProcessor(minx=0, miny=0, maxx=width, maxy=height))
 
-@pytest.fixture
-@given("a path to 'room3'")
-def path_to_room_3(simulation):
-    path = get_component(simulation, Path, 'robot')
-    return path
-
-@when("the robot receives a move-to Position 2,2 in 'room3'")
-def receive_move_to_2_2(simulation):
+@given("a robot with the ability to move to a specific position")
+def ability_to_move_to_specific_position(simulation):
     width, height = simulation.window_dimensions
     simulation.add_des_system((gotoProcessor.process,))
     simulation.add_system(PathProcessor())
     simulation.add_system(MovementProcessor(minx=0, miny=0, maxx=width, maxy=height))
 
-@when("the robot receives a move-to end of path in 'room3'")
-def receive_move_to_end_of_path(simulation):
-    width, height = simulation.window_dimensions
-    simulation.add_system(PathProcessor())
-    simulation.add_system(MovementProcessor(minx=0, miny=0, maxx=width, maxy=height))
+@given("a goto event to the center of 'room three'")
+def goto_event_to_center_of_room_three(simulation, robot):
+    room_three_center = get_center(simulation, 'room3')
+    store_goto_position_event(simulation, robot, room_three_center)
+
+@given("a path from the robot to the center of 'room three'")
+def path_to_room_three(simulation, robot):
+    room_three_center = get_center(simulation, 'room3')
+    robot_center = get_component(simulation, Position, robot).center
+    points = [robot_center, room_three_center]
+    create_path(simulation, robot, points)
 
 @when("after run simulation")
-def run_simulation(simulation, path_to_room_3):
+def run_simulation(simulation):
     simulation.run()
 
-@then("the robot is in Position 2,2 of 'room3'")
-def assert_robot_is_in_position_2_2(simulation):
-    position = get_component(simulation, Position, 'robot')
-    assert position.center[0] == 2
-    assert position.center[1] == 2
+@then("the robot is in the center of 'room three'")
+def robot_is_in_center_of_room_three(simulation, robot):
+    robot_position = get_component(simulation, Position, robot)
+    room_three_center = get_center(simulation, 'room3')
 
-@then("the robot is in the end of path in 'room3'")
-def assert_robot_is_in_end_of_path(simulation, path_to_room_3):
-    position = get_component(simulation, Position, 'robot')
-    target_path = get_path_last_point(path_to_room_3)
-    assert target_path['x'] == position.center[0]
-    assert target_path['y'] == position.center[1]
+    assert robot_position.center[0] == room_three_center[0]
+    assert robot_position.center[1] == room_three_center[1]
