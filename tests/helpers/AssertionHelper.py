07431053dac5b8fea45e2f742ed53bdd62c4212d
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-03-14 16:57:28 -0300

    Improve documentation

--- tests/helpers/AssertionHelper.py
+++ tests/helpers/AssertionHelper.py
@@ -3,10 +3,11 @@ from tests.helpers.TestHelper import TestHelper
 
 class AssertionHelper(TestHelper):
     def __init__(self, simulation) -> None:
+        """Receives a Simulation instance as a parameter."""
         super().__init__(simulation)
 
-    def have_collided(self, entity_id, other_entity_id):
-        """Verifica se ocorreu 1 colisao."""
+    def have_collided(self, entity_id: str, other_entity_id: str):
+        """Assert that a collision has occurred between the entity and the other_entity."""
         collision = self.get_component(Collision, entity_id)
         other_entity_id = self.cast_id(other_entity_id)
 
@@ -14,23 +15,34 @@ class AssertionHelper(TestHelper):
             return True
         return False
 
-    def is_in_poi(self, entity_id, poi_tag):
-        entity_position = self.get_center(entity_id)
+    def is_in_poi(self, drawio_id: str, poi_tag: str):
+        """
+        Assert if the center of the object is positioned at the poi (point of interest).
+
+        - drawio_id: property id from an element of the drawio file.
+        - poi_tag: the poi_tag property of a POI
+        """
+        entity_center = self.get_center(drawio_id)
         poi = self.get_poi(poi_tag)
         
-        if (entity_position[0] == poi[0]) and (entity_position[1] == poi[1]):
+        if (entity_center[0] == poi[0]) and (entity_center[1] == poi[1]):
             return True
         else:
             return False
             # TODO: raise AssertionError(f'Entity is not in the POI. Entity Position: {entity_position}. Poi: {poi}')
             # como retornar o Assertion e o False
 
-    def robot_drop_pickable_in_poi(self, robot_id, pickable_name, poi_tag):
+    def robot_drop_pickable_in_poi(self, robot_id: str, pickable_name: str, poi_tag: str):
+        """
+        Assert if the pickable is in the poi that the robot has left.
+
+        - robot_id: drawio id of the robot that held the pickable.
+        - pickable_name: the name property of the grabbed object, typed 'pickable'.
+        - poi_tag: the tag property of the destination poi.
+        """
         pickable_position = self.get_center(pickable_name)
         poi = self.get_poi(poi_tag)
         robot_height = self.get_position(robot_id).h
-
-        print("robot height: ", robot_height)
 
         if (pickable_position[0] == poi[0]) and (pickable_position[1] == poi[1] + robot_height):
             return True
@@ -38,7 +50,11 @@ class AssertionHelper(TestHelper):
             return False
 
     def is_in_center_of(self, entity_id, other_entity_id):
-        """Compara o x e o y do centro."""
+        """
+        Asserts that the center of entity is the same center of the other_entity.
+
+        - entity_id, other_entity_id: the property id of the element (drawio file).
+        """
         entity_center = self.get_center(entity_id)
         other_entity_center = self.get_center(other_entity_id)
 
@@ -47,7 +63,11 @@ class AssertionHelper(TestHelper):
         return False
     
     def is_in_same_position(self, entity_id, other_entity_id):
-        """Compara o x e o y."""
+        """
+        Asserts that the (x,y) position of entity is the same of the other_entity.
+
+        - entity_id, other_entity_id: the property id of the element (drawio file).
+        """
         entity_position = self.get_position(entity_id)
         other_entity_position = self.get_position(other_entity_id)
 
@@ -56,7 +76,12 @@ class AssertionHelper(TestHelper):
         return False
     
     def is_in_position(self, entity_id, position):
-        # TODO: melhorar nome dos metodos
+        """
+        Asserts that the entity is int the (x, y) position passed as parameter.
+
+        - entity_id: the property id of the element (drawio file).
+        - position: a coordinate (x, y) of the map.
+        """
         entity_position = self.get_position(entity_id)
         if entity_position.x == position[0] and entity_position.y == position[1]:
             return True
--- tests/helpers/TestHelper.py
+++ tests/helpers/TestHelper.py
@@ -1,13 +1,19 @@
 from simulator.components.Position import Position
 from simulator.components.Collision import Collision
 from simulator.components.Map import Map
+from typehints.component_types import Component
 
 class TestHelper:
     def __init__(self, simulation):
+        """Receives a Simulation instance as a parameter."""
         self.simulation = simulation
 
-    def get_object_id(self, drawio_id):
-        """Gets the simulation entity id given the id of the drawio object."""
+    def get_object_id(self, drawio_id: str) -> int:
+        """
+        Gets the esper entity id from a drawio object id.
+
+        - drawio_id: property id from an object element tree.
+        """
         drawio_id = str(drawio_id)
 
         for obj in self.simulation.objects:
@@ -15,21 +21,31 @@ class TestHelper:
                 return obj[0]
         return None
 
-    def get_drawio_id(self, drawio_id):
+    def get_drawio_id(self, drawio_id: str) -> int:
+        """
+        Gets the esper entity id from a drawio 'draw2ent' id.
+
+        - drawio_id: property id of an style element (is not an object) of drawio file (xml).
+        """
         entity_id = self.simulation.draw2ent.get(drawio_id, None)
         if entity_id:
             return entity_id[0]
         return entity_id
 
-    def get_pickable_id(self, name):
+    def get_pickable_id(self, name: str) -> int:
+        """
+        Gets the esper entity id from a pickable name.
+
+        - name: property of an element tree of type 'pickable'.
+        """
         return self.simulation.interactive.get(name, None)
 
-    def cast_id(self, drawio_id):
-        """Converts the object id present in the xml tree to the object id in the simulation.
+    def cast_id(self, drawio_id: str) -> int:
+        """
+        Transforms the id present in the drawio xml to the esper entity id.
 
-        Tipos possíveis:
-        - No caso do objeto ser do tipo pickable, passar a propriedade name como
-        parametro para o drawio_id
+        - In case the object is a pickable, pass the name property as the 
+        parameter to the drawio_id.
         """
         entity_id = self.get_object_id(drawio_id)
         if entity_id:
@@ -40,33 +56,62 @@ class TestHelper:
         return self.get_drawio_id(drawio_id)
 
 
-    def get_component(self, component, drawio_id):
+    def get_component(self, component: Component, drawio_id: str) -> Component:
+        """
+        Gets the component from the entity.
+
+        - component: the component class.
+        - drawio_id: property id from an element of the drawio file.
+        """
         entity_id = self.cast_id(drawio_id)
         if entity_id and self.simulation.world.has_component(entity_id, component):
             return self.simulation.world.component_for_entity(entity_id, component)
         else:
             return None
 
-    def get_position(self, drawio_id):
+    def get_position(self, drawio_id: str) -> Position:
+        """
+        Gets the Position component from an entity.
+
+        - drawio_id: property id from an element of the drawio file.
+        """
         return self.get_component(Position, drawio_id)
     
-    def set_position(self, drawio_id, x, y):
+    def set_position(self, drawio_id: str, x: float, y: float) -> None:
+        """
+        Changes the position of an entity.
+
+        - drawio_id: property id from an element of the drawio file.
+        - x: the new x-axis value.
+        - y: the new y-axis value
+        """
         position = self.get_component(Position, drawio_id)
         if position:
             position.x = x
             position.y = y
 
-    def get_center(self, drawio_id):
+    def get_center(self, drawio_id: str):
+        """
+        Returns a tuple with the center position (x,y) of the entity.
+
+        - drawio_id: property id from an element of the drawio file.
+        """
         position = self.get_component(Position, drawio_id)
         if position:
             return position.center
         return None
     
-    def get_poi(self, poi_tag):
+    def get_poi(self, poi_tag: str):
+        """Returns a tuple with the position values (x,y) of the POI."""
         world_map = self.simulation.world.component_for_entity(1, Map)
         return world_map.pois[poi_tag]
 
-    def get_collision(self, entity_id, other_entity_id):
+    def get_collision(self, entity_id: str, other_entity_id: str) -> Collision:
+        """
+        Gets the collision event that occurred between the entity and the other_entity.
+
+        - entity_id, other_entity_id: the property id of the element (drawio file).
+        """
         collision = self.get_component(Collision, entity_id)
         other_entity_id = self.cast_id(other_entity_id)
 
