4456dbcd8ea22a074656d7c85125f000c23ab295
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-04-05 10:37:19 -0300

    multi robot camera scenario

--- tests/step_defs/test_camera.py
+++ tests/step_defs/test_camera.py
@@ -1,5 +1,5 @@
 import pytest
-from pytest_bdd import scenarios, given, when, then
+from pytest_bdd import scenarios, given, when, then, parsers
 from main import Simulator
 from tests.helpers.ScenarioCreationHelper import ScenarioCreationHelper
 from tests.helpers.AssertionHelper import AssertionHelper
@@ -24,36 +24,36 @@ def scenario_helper(simulation):
 def assertion_helper(simulation):
     return AssertionHelper(simulation)
 
-@given("a map containing a robot with a camera", target_fixture="simulation")
+@given("a map containing robots with camera components", target_fixture="simulation")
 def map_robot_with_camera(config):
     config["map"] = "camera_map.drawio"
     simulation = Simulator(config)
     return simulation
 
-@given("a robot with a camera component")
-def add_camera_to_robot(scenario_helper):
-    scenario_helper.add_camera('robot')
+@given(parsers.parse("a robot with id '{robot}' that has a camera component"))
+def add_camera_to_robot(scenario_helper, robot):
+    scenario_helper.add_camera(robot)
 
 @given("all the simulation robots has detection ability")
 def recognition_ability(scenario_helper: ScenarioCreationHelper):
     scenario_helper.add_detection_ability()
 
-@given("a camera event to detect a 'person1' that is in the camera field of view")
-def add_event_to_detect_person1(scenario_helper: ScenarioCreationHelper):
-    scenario_helper.add_camera_detection_event('robot', 'person1')
+@given(parsers.parse("a camera event for robot '{robot}' detect a '{person}' that is in the camera field of view"))
+def add_event_to_detect_person1(scenario_helper: ScenarioCreationHelper, robot, person):
+    scenario_helper.add_camera_detection_event(robot, person)
 
-@given("a camera event to detect a 'person2' that is not in the camera field of view")
-def add_event_to_detect_person2(scenario_helper: ScenarioCreationHelper):
-    scenario_helper.add_camera_detection_event('robot', 'person2')
+@given(parsers.parse("a camera event for robot '{robot}' detect a '{person}' that is not in the camera field of view"))
+def add_event_to_detect_person2(scenario_helper: ScenarioCreationHelper, robot, person):
+    scenario_helper.add_camera_detection_event(robot, person)
 
 @when("after run simulation")
 def run_simulation(simulation):
     simulation.run()
 
-@then("information about the entity 'person1' is detected by the camera")
-def captured_the_persons_information(assertion_helper):
-    assert assertion_helper.captured_camera_info('robot', 'person1') is True
+@then(parsers.parse("information about the entity '{person}' was detected by the '{robot}' robot camera"))
+def captured_the_persons_information(assertion_helper, robot, person):
+    assert assertion_helper.captured_camera_info(robot, person) is True
 
-@then("information about the entity 'person2' is not detected by the camera")
-def did_not_captured_the_persons_information(assertion_helper):
-    assert assertion_helper.captured_camera_info('robot', 'person2') is False
+@then(parsers.parse("information about the entity '{person}' was not detected by the '{robot}' robot camera"))
+def did_not_captured_the_persons_information(assertion_helper, robot, person):
+    assert assertion_helper.captured_camera_info(robot, person) is False
