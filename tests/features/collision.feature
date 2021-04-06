f99f9f17ad26fa237b45537195e273d0d5b23abd
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-04-06 13:42:54 -0300

    multi-robot scenario for collision

--- tests/features/collision.feature
+++ tests/features/collision.feature
@@ -7,22 +7,35 @@ Feature: Collision
 
 # Map: tests/data/collidable_wall_map.drawio
 # Map description: Consists of one room with colidable walls and
-# a robot that has a path that collides with the wall
+# two robots that has a path that collides with the wall
 
 Scenario: A collision event is received when a collision occurs
     Given a map with a collidable wall
-    And a robot with ability to collide with the wall
-    And a robot with the ability to follow a path
-    And a path from the robot to the collidable wall
+    And all the simulations robots has the ability to collide with the wall
+    And all the simulation robots has the ability to follow a path
+    And a path from the 'robot' to the 'collidable_wall'
     When after run simulation
-    Then the robot collides with the wall
+    Then the 'robot' collides with the 'collidable_wall'
 
 Scenario: Two collision events is received when two collisions occurs
     Given a map with a collidable wall
-    And a robot with ability to collide with the wall
-    And a robot with the ability to follow a path
-    And a path from the robot to the collidable wall
-    And a path from the robot to the second collidable wall
+    And all the simulations robots has the ability to collide with the wall
+    And all the simulation robots has the ability to follow a path
+    And a path from the 'robot' to the 'collidable_wall'
+    And a path from the 'robot' to the 'second_collidable_wall'
     When after run simulation
-    Then the robot collides with the wall
-    Then the robot collides with the second wall
+    Then the 'robot' collides with the 'collidable_wall'
+    Then the 'robot' collides with the 'second_collidable_wall'
+
+Scenario: Two collision events is received when two differents robots collides with a wall
+    Given a map with a collidable wall
+    And all the simulations robots has the ability to collide with the wall
+    And all the simulation robots has the ability to follow a path
+    And a path from the 'robot' to the 'collidable_wall'
+    And a path from the 'robot2' to the 'second_collidable_wall'
+    When after run simulation
+    Then the 'robot' collides with the 'collidable_wall'
+    Then the 'robot2' collides with the 'second_collidable_wall'
+
+# outro cenario: robot1 colide com a wall e robot 2 collide com a second wall
+# Guarda apenas a ultima colisao entre duas entidades
--- tests/step_defs/test_collision.py
+++ tests/step_defs/test_collision.py
@@ -1,5 +1,5 @@
 import pytest
-from pytest_bdd import scenarios, given, when, then
+from pytest_bdd import scenarios, given, when, then, parsers
 from main import Simulator
 
 from tests.helpers.ScenarioCreationHelper import ScenarioCreationHelper
@@ -31,35 +31,25 @@ def map_with_colliding_wall(config):
     simulation = Simulator(config)
     return simulation
 
-@given("a robot with ability to collide with the wall")
-def ability_to_collide(scenario_helper):
+@given("all the simulations robots has the ability to collide with the wall")
+def ability_to_collide(scenario_helper: ScenarioCreationHelper):
     scenario_helper.add_ability_to_collide()
 
-@given("a robot with the ability to follow a path")
-def ability_to_follow_path(scenario_helper):
+@given("all the simulation robots has the ability to follow a path")
+def ability_to_follow_path(scenario_helper: ScenarioCreationHelper):
     scenario_helper.add_ability_to_follow_path()
 
-@given("a path from the robot to the collidable wall")
-def path_to_collidable_wall(scenario_helper):
-    robot_center = scenario_helper.get_center('robot')
-    wall_center = scenario_helper.get_center('collidable_wall')
+@given(parsers.parse("a path from the '{robot}' to the '{collidable_wall}'"))
+def path_to_collidable_wall(scenario_helper: ScenarioCreationHelper, robot, collidable_wall):
+    robot_center = scenario_helper.get_center(robot)
+    wall_center = scenario_helper.get_center(collidable_wall)
     points = [robot_center, wall_center]
-    scenario_helper.create_path('robot', points)
-
-@given("a path from the robot to the second collidable wall")
-def path_to_second_collidable_wall(scenario_helper):
-    wall_center = scenario_helper.get_center('second_collidable_wall')
-    points = [wall_center]
-    scenario_helper.create_path('robot', points)
+    scenario_helper.create_path(robot, points)
 
 @when("after run simulation")
 def run_simulation(simulation):
     simulation.run()
 
-@then("the robot collides with the wall")
-def collided_into_the_wall(assertion_helper):
-    assert assertion_helper.have_collided('robot', 'collidable_wall')
-
-@then("the robot collides with the second wall")
-def collided_into_second_wall(assertion_helper):
-    assert assertion_helper.have_collided('robot', 'second_collidable_wall')
+@then(parsers.parse("the '{robot}' collides with the '{collidable_wall}'"))
+def collided_into_the_wall(assertion_helper: AssertionHelper, robot, collidable_wall):
+    assert assertion_helper.have_collided(robot, collidable_wall)
