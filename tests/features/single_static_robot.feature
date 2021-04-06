8b19716751383c0d4e6e6f933eb0503e7278c366
Cristiane Naves <cristianenavescardoso09@gmail.com>
2021-04-06 10:37:04 -0300

    multi-robot scenario for static robot

--- tests/features/single_static_robot.feature
+++ tests/features/single_static_robot.feature
@@ -10,6 +10,16 @@ Feature: Single Static Robot
 
 Scenario: Single static robot
     Given a map with one room
-    And a robot in position 1,1 inside the room
+    And a robot with id 'robot' in position '1,1' inside the room
     When the robot stay still 
-    Then the robot is in 1,1
+    Then the robot with id 'robot' is in '1,1'
+
+Scenario: Multi static robots
+    Given a map with one room
+    And a robot with id 'robot' in position '1,1' inside the room
+    And a robot with id 'robot2' in position '25,25' inside the room
+    And a robot with id 'robot3' in position '42,42' inside the room
+    When the robot stay still
+    Then the robot with id 'robot' is in '1,1'
+    Then the robot with id 'robot2' is in '25,25'
+    Then the robot with id 'robot3' is in '42,42'
--- tests/step_defs/test_single_static_robot.py
+++ tests/step_defs/test_single_static_robot.py
@@ -1,5 +1,5 @@
 import pytest
-from pytest_bdd import scenarios, given, when, then
+from pytest_bdd import scenarios, given, when, then, parsers
 from main import Simulator
 
 from tests.helpers.ScenarioCreationHelper import ScenarioCreationHelper
@@ -27,14 +27,16 @@ def map_with_one_room():
     simulation = Simulator(config)
     return simulation
 
-@given("a robot in position 1,1 inside the room")
-def a_robot_in_position_1_1(scenario_helper):
-    scenario_helper.set_position('robot', 1, 1)
+@given(parsers.parse("a robot with id '{robot}' in position '{position}' inside the room"))
+def a_robot_in_given_position(scenario_helper: ScenarioCreationHelper, robot, position):
+    pos = position.replace(' ', '').split(',')
+    scenario_helper.set_position(robot, int(pos[0]), int(pos[1]))
 
 @when("the robot stay still")
 def stay_still(simulation):
     simulation.run()
 
-@then("the robot is in 1,1")
-def check_the_robot_is_in_1_1(assertion_helper):
-    assert assertion_helper.is_in_position('robot', (1,1))
+@then(parsers.parse("the robot with id '{robot}' is in '{position}'"))
+def check_the_robot_is_in_position(assertion_helper: AssertionHelper, robot, position):
+    pos = position.replace(' ', '').split(',')
+    assert assertion_helper.is_in_position(robot, (int(pos[0]),int(pos[1])))
